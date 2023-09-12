df2e2e94b828051505ce7890ab8def41e33d3599
Nathaniel Geiger <Nathaniel-Geiger@live.nmit.ac.nz>
2023-09-12 12:25:58 +1200

    changed to line counts by type

--- dom-events/dom.js
+++ dom-events/dom.js
@@ -1,16 +1,30 @@
 //NOTE if no id is specified it will return a NULL response
+let btn = document.getElementById('btnCount')
+btn.addEventListener('click',() => {
+    let hTwoHeader = document.getElementsByTagName('h2')
+    alert(`The number of H2 tags are: ${hTwoHeader.length}`)
+})
+
+
+
+
+
+
+
+
+
 // const p = document.getElementById('message'); // querySelector() can be used for general types such as <p> or <h1>
 // console.log(p);
 
-let btn = document.getElementsById('btnRadio')
-let output = document.getElementById('output')
+// let btn = document.getElementById('btnRadio')
+// let output = document.getElementById('output')
 
 
-btn.addEventListener('click', () => {
-    let languages = document.getElementsByName('language')
-        languages.forEach((language) => {
-            if(language.checked) {
-                output.innerText = `You code with : ${language.value}`
-            }
-        })
-})
+// btn.addEventListener('click', () => {
+//     let languages = document.getElementsByName('language')
+//         languages.forEach((language) => {
+//             if(language.checked) {
+//                 output.innerText = `You code with : ${language.value}`
+//             }
+//         })
+// })
