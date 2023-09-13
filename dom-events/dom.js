7beab0894cb7d68a8fc05b4e9e9e593aa3fdbbc0
Nathaniel Geiger <Nathaniel-Geiger@live.nmit.ac.nz>
2023-09-14 10:39:19 +1200

    sibling chain

--- dom-events/dom.js
+++ dom-events/dom.js
@@ -1,11 +1,45 @@
-//NOTE if no id is specified it will return a NULL response
-let menu = document.getElementByClassId('menu')
-let items = menu.getElementsByClassName('item')
+// let current = document.querySelector('.current')
+// let nextSibling = current.nextElementSibling
+
+// while(nextSibling) {
+//     nextSibling = nextSibling.nextElementSibling
+// }
+
+// let current = document.querySelector('.current')
+// let prevSibling = current.prevElementSibling
+
+let getSiblings = function(e) {
+    let siblings = []
+
+    if(!e.parentNode) {
+        return siblings
+    }
+    let sibling = e.parentNode.firstChild
+
+    while(sibling) {
+        if(sibling.nodeType == 1 && !== e) {
+            siblings.push(sibling)
+        }
+        sibling = sibling.nextSibling
+    }
+    return siblings
+}
+// //NOTE - this first comment sets up the note status 
+// let note = document.querySelector('.note')
+
+// //NOTE - this gets the note to read by target
+// console.log(note.parentnode)
 
 
-let data = [].map.call(items, item => item.textContent) // ['JavaScript', 'CSS', 'HTML', 'TypeScript']
+
+// let menu = document.getElementByClassId('menu')
+// let items = menu.getElementsByClassName('item')
+
+
+// let data = [].map.call(items, item => item.textContent) // ['JavaScript', 'CSS', 'HTML', 'TypeScript']
 //NOTE - this builds an array for storing data selected by class and content
 
+//NOTE if no id is specified it will return a NULL response
 
 // let btn = document.getElementById('btnCount')
 // btn.addEventListener('click',() => {
