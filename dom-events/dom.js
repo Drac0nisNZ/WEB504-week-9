538661a0026a726ed96de538312df6348aa7ded6
Nathaniel Geiger <Nathaniel-Geiger@live.nmit.ac.nz>
2023-09-21 10:03:37 +1200

    finished

--- dom-events/dom.js
+++ dom-events/dom.js
@@ -13,20 +13,97 @@ const cover = document.getElementById('cover')
 
 const songs = ['blue-reeves', 'slow-snow', 'the-roads']
 
+//keep track of song
 let songIndex = 2
 
-//Play song
+loadSong(songs[songIndex])
 
+function loadSong(song) {
+    title.innerText = song
+    audio.src = `assets/audio/${song}.mp3`
+    cover.src = `assets/images/${song}.jpg`
+}
+
+//Play song
 function playSong() {
     musicContainer.classList.add('play')
     playBtn.querySelector('i.fas').classList.remove('fa-play')
     playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
 }
 
 //Pause song
-
 function pauseSong() {
     musicContainer.classList.remove('play')
     playBtn.querySelector('i.fas').classList.add('fa-play')
     playBtn.querySelector('i.fas').classList.remove('fa-play')
+
+    audio.pause()
 }
+
+//prev button
+function prevSong() {
+    songIndex-- 
+
+    if(songIndex < 0) {
+        songIndex = songs.length-1
+    }
+}
+
+//loadSong()
+loadSong(songs[songIndex]) 
+
+//playSong()
+
+function nextSong() {
+    songIndex++
+
+    if(songIndex > songs.length-1) {
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+
+    playSong()
+}
+
+//Progress bar setup
+
+function updateProgress(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime/ duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e) {
+    const width = this.clientWidth
+    const clickX = e.offestX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width) * duration
+}
+
+
+
+
+
+//Event Listener
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong()  
+    } else {
+        playSong()
+    }
+})
+
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
